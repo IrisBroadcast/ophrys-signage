63aa1462e579d7e9a3413bbfc7db9edced82586c
Jim Eld <jim.eld@sr.se>
2019-11-11 23:36:56 +0100

    minor opt - removed obsolete lines dpkg helpscript

--- storage/scripts/dpkg-helpscript.sh
+++ storage/scripts/dpkg-helpscript.sh
@@ -7,10 +7,8 @@ DEVICE=/usr/local/aloe/conf/device.txt
 
 function safeApt ()
 {
-    #################
     FUNC_STATUS="Uncomplete function. "$1" triggered DPKG helpscript"
     functionInfo
-    #################
 
     ## Might be due to packet problem...
     dpkg --configure -a
@@ -21,21 +19,17 @@ function safeApt ()
     else
         echo "not ok result, will rm update folder and continuing"
         rm -r /var/lib/dpkg/updates/*
-            if apt-get update
-            then
-                echo "Running apt-get update is OK - That was one of the problems"
-                #################
-                FUNC_STATUS="apt-get update was possible after repair"
-                FUNC_REPAIR_PACKAGES=true
-                functionInfo
-                #################
-            else
-                echo "apt-get update not possible eventhough repair - needs manual intervention"
-                #################
-                FUNC_STATUS="apt-get update not possible eventhough repair - needs manual intervention"
-                functionInfo
-                #################
-            fi
+        if apt-get update
+        then
+            echo "Running apt-get update is OK - That was one of the problems"
+            FUNC_STATUS="apt-get update was possible after repair"
+            FUNC_REPAIR_PACKAGES=true
+            functionInfo
+        else
+            echo "apt-get update not possible eventhough repair - needs manual intervention"
+            FUNC_STATUS="apt-get update not possible eventhough repair - needs manual intervention"
+            functionInfo
+        fi
     fi
 
     ## Or a corrupt file problem
@@ -47,19 +41,19 @@ function safeApt ()
     #break
     output=$(file ${listfile}| awk '{print $2}')
     #echo "${output}"
-        if [[ $output == *"data"* ]]
-        then
-            echo "${listfile} is CORRUPT!"
-            echo "mv $listfile "${listfile}.broken" || exit $?"
-            mv $listfile "${listfile}.broken"
-            #################
-            FUNC_STATUS="Found Broken file"
-            FUNC_REPAIR_FILES=true
-            BROKENFILE=$listfile
-            functionInfo
-            #################
-            ##Creating a new empy file
-            touch $listfile "${listfile}"  || exit $?
-        fi
+    if [[ $output == *"data"* ]]
+    then
+        echo "${listfile} is CORRUPT!"
+        echo "mv $listfile "${listfile}.broken" || exit $?"
+        mv $listfile "${listfile}.broken"
+        #################
+        FUNC_STATUS="Found Broken file"
+        FUNC_REPAIR_FILES=true
+        BROKENFILE=$listfile
+        functionInfo
+        #################
+        ##Creating a new empy file
+        touch $listfile "${listfile}"  || exit $?
+    fi
     done
 }
